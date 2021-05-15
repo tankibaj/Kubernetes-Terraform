8f81552b7c5270ea7f03b1e6457ff9970c2f6a6a
Naim <mail@thenaim.com>
2021-05-15 02:38:34 +0200

    add service clusterip

--- service.tf
+++ service.tf
@@ -8,11 +8,10 @@ resource "kubernetes_service" "whoami" {
       App = kubernetes_deployment.whoami.spec.0.template.0.metadata[0].labels.App
     }
     port {
-      node_port   = 30201
       port        = 80
       target_port = 80 #container_port
     }
 
-    type = "NodePort"
+    type = "ClusterIP"
   }
 }
